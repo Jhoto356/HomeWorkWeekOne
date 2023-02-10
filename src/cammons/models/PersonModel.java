05b169fbd35c3fdf2889a88efe756d560051ab37
jhonatan.arias <jhonatan.arias@onoff.com.co>
2023-02-10 16:24:20 -0500

    set attributes class

--- src/cammons/models/PersonModel.java
+++ src/cammons/models/PersonModel.java
@@ -1,7 +1,32 @@
 package cammons.models;
 
+import cammons.sources.StringES;
+
 public class PersonModel {
     /** ATTRIBUTES **/
+    String name;
+    int age;
+    char genre;
+    double height;
 
+    /** CONSTRUCTOR **/
+    public PersonModel (String pName,  int pAge, char pGenre, double pHeight) {
+        name = pName;
+        age = pAge;
+        genre = pGenre;
+        height = pHeight;
+
+    }
+
+    /** METHODS **/
+    public void sendGreeting() {
+        String message = new StringES().
+
+    }
+
+    private void printShortcut(String finalMessage) {
+        System.out.println(finalMessage);
+
+    }
 
 }
---
+++ src/cammons/sources/StringES.java
@@ -0,0 +1,11 @@
+package cammons.sources;
+
+public class StringES {
+    public String txtMyName = "Hola mi nombre es ";
+    public String txtMyAge = ", tengo ";
+    public String txtYear = " años, ";
+    public String txtMyGenre = "soy ";
+    public String txtMyHeight = ", y mido ";
+    public String txtPointFinal = ".";
+
+}
